de2309d0114ba0f45eea89652801100928ab46d9
Chris Yuen <chris@kizzx2.com>
2014-10-02 13:41:44 +0800

    Support normal (non-chunked) Transfer-Encoding

--- http-gunzip.rb
+++ http-gunzip.rb
@@ -6,29 +6,45 @@ require 'stringio'
 dat = $stdin.read.force_encoding('ASCII-8BIT')
 i0 = 0
 
-while i = dat.index(/^content-encoding: gzip\r?$/i, i0)
+while i = dat.index(%r(^HTTP/1\.[01] \d+ [a-zA-Z ]+\r?$), i0)
   print dat[i0, i - i0]
 
   m = dat.match(/^\r?$/, i)
   j = m.offset(0)[1] + 1
 
-  print dat[i, j - i]
+  headers = dat[i, j - i]
+  print headers
 
-  loop do
-    m = dat.match(/^([0123456789abcdef]+)?\r?$/, j)
-    l = m[1].to_i(16) rescue 0
+  gzip_encoding = headers.lines.grep(/^content-encoding: gzip/i).first
+  if gzip_encoding.nil?
+    i0 = j
+    next
+  end
 
-    if l == 0
-      i0 = m.offset(0)[1] + 1
-      break
-    end
+  content_length = headers.lines.grep(/^content-length: \d+\r?$/i).first
+  content_length = content_length.split.last.to_i if content_length
 
-    z = dat[m.offset(0)[1] + 1, l]
-
-    profit = Zlib::GzipReader.new(StringIO.new(z)).read
+  if content_length
+    profit = Zlib::GzipReader.new(StringIO.new(dat[j, content_length])).read
     print profit
+    i0 = j + content_length
+  else # Assume chunked encoding
+    loop do
+      m = dat.match(/^([0123456789abcdef]+)?\r?$/, j)
+      l = m[1].to_i(16)
 
-    j = m.offset(0)[1] + l + 2
+      if l == 0
+        i0 = m.offset(0)[1] + 1
+        break
+      end
+
+      z = dat[m.offset(0)[1] + 1, l]
+
+      profit = Zlib::GzipReader.new(StringIO.new(z)).read
+      print profit
+
+      j = m.offset(0)[1] + l + 2
+    end
   end
 
   puts
